36c865f52c6aa2771e026befe40daeb935dc71c4
qq2635 <qq2635@naver.com>
2022-06-16 17:18:05 +0900

    계층형 클래스로 OjbectMapper 가져오기

--- src/main/java/egovframework/example/sample/web/Body.java
+++ src/main/java/egovframework/example/sample/web/Body.java
@@ -1,5 +1,6 @@
 package egovframework.example.sample.web;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import lombok.Data;
@@ -12,8 +13,9 @@ import lombok.ToString;
 @NoArgsConstructor
 public class Body {
 	
-	 List<Items> items;
-	//private Items items;
+	/*이상하게 배열형식이지만 List는 안받고 Object 형식으로 받으니 받아진다.*/
+	 //List<Items> items;
+	Object items;
 	private String pageNo;
 	private String totalCount;
 	private String numOfRows;
