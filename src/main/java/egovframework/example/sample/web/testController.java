e73bfa87d59e4360ebc50aa237c9b1446032c5db
qq2635 <qq2635@naver.com>
2022-07-08 17:32:27 +0900

    제공된 데이터 엑셀로 받기 1차 완

--- src/main/java/egovframework/example/sample/web/testController.java
+++ src/main/java/egovframework/example/sample/web/testController.java
@@ -34,6 +34,18 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import egovframework.example.sample.service.impl.dataMapper;
 import egovframework.example.sample.service.impl.staticsMapper;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+import java.util.Set;
+import java.util.TreeMap;
+
 @Controller
 public class testController {
 	@Resource(name = "dataMapper")
@@ -664,6 +676,8 @@ public class testController {
 		return obj.toJSONString();	
 	}
 	
+	public static String filePath = "C:\\poi_temp";
+	public static String fileNm = "poi_making_file_test.xlsx";
 	@RequestMapping(value = "/gridExcelDownload.do",
 			produces = "application/text; charset=UTF-8")
 	@ResponseBody
@@ -682,6 +696,84 @@ public class testController {
 		
 		decodeVal = decodeVal.replace("jsonData=", "");
 		System.out.println("치환: " + decodeVal);
+		
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		List<Map<String, Object>> bodyList =  new ArrayList<>();
+		
+		try {
+			Body desi = objectMapper.readValue(decodeVal, Body.class);
+			
+			bodyList = (List<Map<String, Object>>)desi.getItems();
+		}catch(Exception e) {
+			e.printStackTrace();
+			System.out.println("아이템 직렬화로 읽기 중 오류");
+		}
+		
+		// 빈 Workbook 생성
+        XSSFWorkbook workbook = new XSSFWorkbook();
+
+        // 빈 Sheet를 생성
+        XSSFSheet sheet = workbook.createSheet("Nutrient data");
+
+        // Sheet를 채우기 위한 데이터들을 Map에 저장
+        Map<String, Object[]> data = new TreeMap<>();
+        
+        for(int i = 0; i<bodyList.size();i++) {
+			LinkedHashMap<String, Object> lmap = (LinkedHashMap<String, Object>) bodyList.get(i);
+			//System.out.println("bodyList 각 요소: "+lmap);
+			System.out.println(lmap.get("DESC_KOR"));
+			//lmap.get("DESC_KOR");
+			String num="";
+			num = String.valueOf(i+1);
+			
+			String DESC_KOR =  String.valueOf(lmap.get("DESC_KOR"));
+			String SERVING_WT =  String.valueOf(lmap.get("SERVING_WT"));
+			String NUTR_CONT1 =  String.valueOf(lmap.get("NUTR_CONT1"));
+			String NUTR_CONT2 =  String.valueOf(lmap.get("NUTR_CONT2"));
+			String NUTR_CONT3 =  String.valueOf(lmap.get("NUTR_CONT3"));
+			String NUTR_CONT4 =  String.valueOf(lmap.get("NUTR_CONT4"));
+			
+			if(num.equals("1")) {
+				data.put(num, new Object[]{"식품이름","1회 제공량(g)", "칼로리(kcal)", "탄수화물(g)","단백질(g)","지방(g)"});
+			}else {
+				data.put(num, new Object[]{DESC_KOR,SERVING_WT, NUTR_CONT1, NUTR_CONT2, NUTR_CONT3, NUTR_CONT4});
+			}
+		}
+		/*
+		 * data.put("1", new Object[]{"ID", "NAME", "PHONE_NUMBER"}); data.put("2", new
+		 * Object[]{"1", "cookie", "010-1111-1111"}); data.put("3", new Object[]{"2",
+		 * "sickBBang", "010-2222-2222"}); data.put("4", new Object[]{"3", "workingAnt",
+		 * "010-3333-3333"}); data.put("5", new Object[]{"4", "wow", "010-4444-4444"});
+		 */
+
+        // data에서 keySet를 가져온다. 이 Set 값들을 조회하면서 데이터들을 sheet에 입력한다.
+        Set<String> keyset = data.keySet();
+        int rownum = 0;
+
+        // 알아야할 점, TreeMap을 통해 생성된 keySet는 for를 조회시, 키값이 오름차순으로 조회된다.
+        for (String key : keyset) {
+            Row row = sheet.createRow(rownum++);
+            Object[] objArr = data.get(key);
+            int cellnum = 0;
+            for (Object obj : objArr) {
+                Cell cell = row.createCell(cellnum++);
+                if (obj instanceof String) {
+                    cell.setCellValue((String)obj);
+                } else if (obj instanceof Integer) {
+                    cell.setCellValue((Integer)obj);
+                }
+            }
+        }
+
+        try {
+            FileOutputStream out = new FileOutputStream(new File(filePath, fileNm));
+            workbook.write(out);
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    
 		return result;
 	}
 	
